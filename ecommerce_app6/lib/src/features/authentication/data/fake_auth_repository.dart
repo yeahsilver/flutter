26ef2055df159387d70a6f5ee30daa65154eb1ac
yeahsilver <gjdpdms2005@gmail.com>
2022-08-25 08:39:13 +0900

    feat: unit test 구현

---
+++ ecommerce_app6/lib/src/features/authentication/data/fake_auth_repository.dart
@@ -0,0 +1,50 @@
+import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
+import 'package:ecommerce_app/src/utils/in_memory_store.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class FakeAuthRepository {
+  final _authState = InMemoryStore<AppUser?>(null);
+
+  Stream<AppUser?> authStateChanges() => _authState.stream;
+  AppUser? get currentUser => _authState.value;
+
+  Future<void> signInWithEmailAndPassword(String email, String password) async {
+    await Future.delayed(const Duration(seconds: 3));
+    if (currentUser == null) {
+      _createNewUser(email);
+    }
+  }
+
+  Future<void> createUserWithEmailAndPassword(
+      String email, String password) async {
+    if (currentUser == null) {
+      _createNewUser(email);
+    }
+  }
+
+  Future<void> signOut() async {
+    // await Future.delayed(const Duration(seconds: 3));
+    // throw Exception('Connection failed');
+    _authState.value = null;
+  }
+
+  void dispose() => _authState.close();
+
+  void _createNewUser(String email) {
+    _authState.value = AppUser(
+      uid: email.split('').reversed.join(),
+      email: email,
+    );
+  }
+}
+
+final authRepositoryProvider = Provider<FakeAuthRepository>((ref) {
+  final auth = FakeAuthRepository();
+  ref.onDispose(() => auth.dispose());
+  return auth;
+});
+
+final authStateChangesProvider = StreamProvider.autoDispose<AppUser?>((ref) {
+  final authRepository = ref.watch(authRepositoryProvider);
+  return authRepository.authStateChanges();
+});
---
+++ ecommerce_app6/test/src/features/products/data/fake_products_repository_test.dart
@@ -0,0 +1,26 @@
+import 'package:ecommerce_app/src/constants/test_products.dart';
+import 'package:ecommerce_app/src/features/products/data/fake_products_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('getProductsList returns global list', () {
+    final productsRepository = FakeProductsRepository();
+    expect(productsRepository.getProductsList(), kTestProducts);
+  });
+
+  test('getProduct(1) returns first item', () {
+    final productsRepository = FakeProductsRepository();
+    expect(
+      productsRepository.getProduct('1'),
+      kTestProducts[0],
+    );
+  });
+
+  test('getProduct(100) returns first item', () {
+    final productsRepository = FakeProductsRepository();
+    expect(
+      () => productsRepository.getProduct('100'),
+      throwsStateError,
+    );
+  });
+}
