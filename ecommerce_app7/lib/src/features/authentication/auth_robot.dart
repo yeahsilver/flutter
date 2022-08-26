e9d274edd3c7bd5b52a7587a9484f80515fe2191
yeahsilver <gjdpdms2005@gmail.com>
2022-08-26 19:23:19 +0900

    feat: find.byKey() 구현

---
+++ ecommerce_app7/lib/src/features/authentication/auth_robot.dart
@@ -0,0 +1,52 @@
+import 'package:ecommerce_app/src/common_widgets/alert_dialogs.dart';
+import 'package:ecommerce_app/src/features/authentication/presentation/account/account_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class AuthRobot {
+  AuthRobot(this.tester);
+  final WidgetTester tester;
+
+  Future<void> pumpAccountScreen() async {
+    await tester.pumpWidget(
+      const ProviderScope(
+        child: MaterialApp(
+          home: AccountScreen(),
+        ),
+      ),
+    );
+  }
+
+  Future<void> tapLogoutButton() async {
+    final finder = find.text("Logout");
+    expect(finder, findsOneWidget);
+    await tester.tap(finder);
+    await tester.pump();
+  }
+
+  // This method does not return an Future because it doesn't use the tester
+  void expectLogoutDialogFound() {
+    final dialogTitle = find.text('Are you sure?');
+    expect(dialogTitle, findsOneWidget);
+  }
+
+  Future<void> tapCancelButton() async {
+    final cancelButton = find.text('Cancel');
+    expect(cancelButton, findsOneWidget);
+    await tester.tap(cancelButton);
+    await tester.pump();
+  }
+
+  void expectLogoutDialogNotFound() {
+    final dialogTitle = find.text('Are you sure?');
+    expect(dialogTitle, findsNothing);
+  }
+
+  Future<void> tapDialogLogoutButton() async {
+    final finder = find.byKey(kDialogDefaultKey);
+    expect(finder, findsOneWidget);
+    await tester.tap(finder);
+    await tester.pump();
+  }
+}
